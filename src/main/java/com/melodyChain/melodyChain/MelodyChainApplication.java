d265fe2479279011486f9255f8f11173912b2204
prushorth <maniprush3@gmail.com>
2025-12-05 11:17:15 -0800

    Add spring initializer

---
+++ src/main/java/com/melodyChain/melodyChain/MelodyChainApplication.java
@@ -0,0 +1,13 @@
+package com.melodyChain.melodyChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melodyChain/melodyChain/MelodyChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodyChain.melodyChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
